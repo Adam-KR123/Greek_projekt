36834ec52325954ec33bcf98526db3b8f3cc86a4
Adam-KR123 <128384355+Adam-KR123@users.noreply.github.com>
2024-05-09 11:27:05 +0200

    lol

---
+++ game.js
@@ -0,0 +1,35 @@
+let start_button=document.getElementById("jatek_start_button");
+start_button.addEventListener("click",(e)=>{
+    document.getElementById("start_div").style.display="none";
+    document.getElementById("jatek_gameboard").style.display="grid";
+})
+let words=["hádész", "zeusz","athéna","apolló","alvilág","olümposz","athén"]
+let letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let used_index=0;
+let used_words=[]
+for( let x=0;x<8;x++){
+    let words_rnd=Math.floor(Math.random()*7)
+    let new_letters=[]
+    if(words[words_rnd] in used_words){}
+    else{
+        new_letters=words[words_rnd].split("")
+        used_words[used_index]=words[words_rnd]
+        used_index++;
+    }
+    let megfel=false;
+    for(let y=0;y<8;y++){
+        let rnd=Math.floor(Math.random()*26);
+        let new_div=document.createElement("div")
+        new_div.style.textAlign="center"
+        new_div.style.fontSize="3vw"
+        new_div.style.display="flex"
+        new_div.style.alignItems="center"
+        new_div.style.justifyContent="center"
+        new_div.style.gridArea=(x+1)+"/"+(y+1);
+        if(new_letters.length+y<8 || megfel){
+            megfel=true;
+            new_div.textContent=new_letters[y]
+        }
+        document.getElementById("jatek_gameboard").appendChild(new_div)
+    }
+}
